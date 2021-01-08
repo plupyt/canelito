f459d15c681a0285cb1d5af14b66178ff3033fcd
Daniel Pérez <plupyt_dany@hotmail.com>
2021-01-08 10:54:49 -0600

    componente Registro agregado con problemas con JSON

---
+++ src/components/Registro.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Registro = () => (
+    <section id="my-hbspt-form">
+        <h2>Regístrate</h2>
+        <div className="form-container">
+            <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+            <script>
+                hbspt.forms.create({
+                    portalId: "8070410", 
+                    formId: "0aa65d8b-d819-4337-9ded-fa9afedfbd1f"
+                })
+            </script>
+        </div>
+    </section>
+)
+
+export default Registro
